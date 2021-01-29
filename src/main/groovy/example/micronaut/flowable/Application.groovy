d33c6e36675026600beeb64bc685f6dad0fbba76
aerodon <don@aerotomic.io>
2021-01-29 08:41:18 -0700

    Removed unused imports.

--- src/main/groovy/example/micronaut/flowable/Application.groovy
+++ src/main/groovy/example/micronaut/flowable/Application.groovy
@@ -1,16 +1,7 @@
 package example.micronaut.flowable
 
-import example.micronaut.flowable.emitters.SensorMeasurementEmitter
-import example.micronaut.flowable.jobs.SensorMeasurementJob
-import io.micronaut.context.ApplicationContext
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
-import io.micronaut.scheduling.TaskScheduler
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import java.time.Duration
-import java.util.concurrent.ScheduledFuture
 
 @CompileStatic
 class Application {
